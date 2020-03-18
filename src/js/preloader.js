1adc81e31ace0a7c30930378a4d336a041dfb318
Sergey Skupow <sergskypow@gmail.com>
2020-03-18 15:23:22 +0200

    ui

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,12 +1,15 @@
-const plain = document.getElementById('plain'),
-plain2 = document.getElementById('plain-2'),
+const plain = [...document.querySelectorAll(".plain")],
 bounce = document.getElementById('bounce'),
-startImg = document.getElementById('start-img');
+startImg = document.getElementById('start-img'),
+menuTop = document.querySelector('.interface'),
+menuBottom = document.querySelector('.menu');
 let tl = new gsap.timeline();
 document.addEventListener("DOMContentLoaded", ()=>{
   tl
-  .from(startImg,2,{scale:0.7,ease:Power4.easeOut,delay:0.5})
-  .to([plain,plain2],2,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.08})
-  .from(bounce,1.5,{scale:1.2,x:0,opacity:0.8,ease: Power4.easeOut,delay:-2})
+  .to(startImg,2,{scale:1.2,ease:Elastic.easeOut.config(1, 0.3),delay:0.5})
+  .to(plain,2.5,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.1})
+  .from(bounce,2,{scale:1.2,x:0,opacity:0.8,ease: Power4.easeOut,delay:-2.7})
+  .from(menuTop,2,{y:"-100%",ease: Power4.easeOut,delay:-1.7})
+  .from(menuBottom,2,{y:"-250%",ease: Power4.easeOut,delay:-1.6})
 })
 //Elastic.easeOut.config(1, 0.3)
