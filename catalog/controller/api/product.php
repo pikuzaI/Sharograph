652e568d6924e0cd4d547a2d188a315726b9459c
root <root@ubuntu>
2020-09-09 16:11:29 +0000

    backup before contentful

--- catalog/controller/api/product.php
+++ catalog/controller/api/product.php
@@ -1,6 +1,10 @@
 <?php
 class ControllerApiProduct extends Controller
 {
+    public function contentful()
+    {
+        
+    }
     public function index()
     {
         $this->load->language('api/cart');
