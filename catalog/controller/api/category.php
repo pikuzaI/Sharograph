31ae0f5c0ba1b03d329e36817de276caa048011a
seregskupow <sergskupow@gmail.com>
2021-05-31 13:05:40 +0000

    before theme refactor

--- catalog/controller/api/category.php
+++ catalog/controller/api/category.php
@@ -1,13 +1,13 @@
 <?php
 class ControllerApiCategory extends Controller {
-	public function getcategories(){
+	public function getcategories() {
 		$this->load->model('catalog/category');
 		$categories = $this->model_catalog_category->getCategoriesFigures();
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($categories));
 	}
 
-	public function test(){
+	public function test() {
 		$this->load->model('catalog/category');
 		$data =  $this->model_catalog_category->test();
 		$this->response->addHeader('Content-Type: application/json');
--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -74,7 +74,7 @@ class ControllerCommonHeader extends Controller {
 		$data['search'] = $this->load->controller('common/search');
 		$data['cart'] = $this->load->controller('common/cart');
 		$data['menu'] = $this->load->controller('common/menu/menu_home');
-
+		//$data['menu'] = $this->load->controller('common/menu');
 		return $this->load->view('common/home_header', $data);
 	}
 	public function index() {
